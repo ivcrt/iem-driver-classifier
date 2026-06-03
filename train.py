be678d73fa206927aeba0729c6fa3a498ea12fb2
ivcrt <76063702+ivcrt@users.noreply.github.com>
2026-06-04 01:44:30 +0200

    Added data loading, grouped split, feature scaling

---
+++ train.py
@@ -0,0 +1,40 @@
+#will try adding cross validation later
+
+
+
+import pandas as pd
+from sklearn.model_selection import GroupShuffleSplit
+from sklearn.preprocessing import StandardScaler
+from collections import Counter
+
+df = pd.read_csv('outputs/iem_driver_dataset.csv')
+
+df = df[df['driver_class'].isin(['DD', 'BA', 'Hybrid'])] # we'll keep only the most represented ones because the others are under-represented
+
+label_map = {'DD': 0, 'BA': 1, 'Hybrid': 2}
+y = df['driver_class'].map(label_map).values
+
+X = df.drop(columns=['model', 'source', 'driver_class', 'driver_raw']).values # so only the frequency response curves
+
+print(X.shape)  
+print(y.shape)  
+groups = df['model'].values  #we need it for groupshufflesplit
+
+
+gss = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=67) #we need to do that because the same iem has measurements ~ 
+#  ~ from different people so we dont want to risk having the same iem in training and in validation 
+
+train_idx, val_idx = next(gss.split(X, y, groups=groups))
+
+X_train, X_val = X[train_idx], X[val_idx]
+y_train, y_val = y[train_idx], y[val_idx]
+
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_val   = scaler.transform(X_val)  
+
+
+print(X_train.shape, X_val.shape)
+print('train:', Counter(y_train))
+print('val:  ', Counter(y_val))
